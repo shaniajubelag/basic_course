5c0b4255c600fa42ef9a01bc8442bafe3dec8f74
shaniajubelag <s98jubelag@gmail.com>
2020-01-23 23:45:58 +0800

    4. Login and Logout

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,17 @@ class User < ApplicationRecord
 
   validates :password, length: { minimum: 6 }
   has_secure_password
+
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.digest(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+    def create_remember_token
+      self.remember_token = User.digest(User.new_remember_token)
+    end
 end

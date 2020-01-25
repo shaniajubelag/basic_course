022b186cd5101d6916449453982fadcf4671efc5
shaniajubelag <s98jubelag@gmail.com>
2020-01-25 19:20:57 +0800

    5. Update and Delete users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+  before_action :only_loggedin_users, only: [:index, :edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
+  def index
+    # @users = User.all
+    @users = User.paginate(page: params[:page], per_page: 10 )
+  end
+
   def new
     @user = User.new
   end
@@ -6,7 +14,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)    # Not the final implementation!
     if @user.save
-      redirect_to @user
+      redirect_to login_url
     else
       render 'new'
     end
@@ -16,8 +24,38 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    redirect_to users_url
+  end
+ 
+
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  # Before filters
+  def only_loggedin_users
+    # Goes to login page UNLESS the user is logged in already
+    redirect_to login_url unless logged_in?
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
   end
 end
